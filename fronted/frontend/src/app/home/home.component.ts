84e2a421cdd85f5d05206936643e019b18f3a9c8
mrgareth <mchirinoss@unsa.edu.pe>
2024-11-26 00:51:59 -0500

    Faltaba el boton de agregar producto

--- fronted/frontend/src/app/home/home.component.ts
+++ fronted/frontend/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { InventoryTableComponent } from "../inventory-table/inventory-table.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,8 @@ import { InventoryTableComponent } from "../inventory-table/inventory-table.comp
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  constructor(private router: Router) { }
+  handleAddProduct() : void{
+    this.router.navigate(['/home/agregarProducto']);
+  }
 }
